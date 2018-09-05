e7bfb0ea425fcadcdfd089c76d7f394a6631864f
Leonel Quinteros <leonel.quinteros@gmail.com>
2018-09-05 11:13:47 -0300

    Add working default values for flag configuration

--- main.go
+++ main.go
@@ -1,17 +1,23 @@
-// amigobot is the default implementation for the amigo IRC bot. 
+// amigobot is the default implementation for the amigo IRC bot.
 package main
 
 import (
 	"flag"
+
 	"github.com/leonelquinteros/amigo"
+	"github.com/leonelquinteros/gorand"
 )
 
 func main() {
+	defaultChannel := "#amigobot"
+	defaultNick, _ := gorand.GetAlphaString(8)
+	defaultPassword, _ := gorand.GetAlphaNumString(32)
+
 	// Load Config
-	host := flag.String("h", "", "Host: The IRC host to connect to")
-	channel := flag.String("c", "", "Channel: The IRC #channel to join after connect to the IRC server")
-	nick := flag.String("n", "", "Nick: The nick to use on the IRC server")
-	password := flag.String("p", "", "Password: The master password to perform authenticated commands without beign a registered master of the bot")
+	host := flag.String("h", "chat.freenode.net:6667", "Host: The IRC host to connect to")
+	channel := flag.String("c", defaultChannel, "Channel: The IRC #channel to join after connect to the IRC server")
+	nick := flag.String("n", defaultNick, "Nick: The nick to use on the IRC server")
+	password := flag.String("p", defaultPassword, "Password: The master password to perform authenticated commands without beign a registered master of the bot")
 	flag.Parse()
 
 	if *host == "" || *channel == "" || *nick == "" || *password == "" {
